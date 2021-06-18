b4d5f1fbb7161633f85afb6109ed343976795a01
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2021-06-18 16:36:40 +0200

    Showing times when page is open and closed

--- resources/views/admin/user-tracking-data/partials/_table-all-data.blade.php
+++ resources/views/admin/user-tracking-data/partials/_table-all-data.blade.php
@@ -3,6 +3,8 @@
     <tr>
         <th scope="col">User Id</th>
         <th scope="col">IP</th>
+        <th scope="col">Visit start</th>
+        <th scope="col">Visit end</th>
         <th scope="col">Duration</th>
     </tr>
     </thead>
@@ -11,6 +13,8 @@
         <tr>
             <td>{{ $data->uuid }}</td>
             <td>{{ $data->ip }}</td>
+            <td>{{ $data->time_start }}</td>
+            <td>{{ $data->time_end }}</td>
             <td>{{ $data->duration }}</td>
         </tr>
     @endforeach
