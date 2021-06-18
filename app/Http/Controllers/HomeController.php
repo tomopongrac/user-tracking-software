45561c0096329e38f1b86ac9aea28a18055cf964
Tomislav Pongrac <25442485+tomopongrac@users.noreply.github.com>
2021-06-18 13:46:55 +0200

    Saving user tracking data and creating cookie

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserTrackingData;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return 'Homepage';
+        $uuid = Str::uuid()->toString();
+
+        // save data
+        $userTrackingData = new UserTrackingData();
+        $userTrackingData->uuid = $uuid;
+        $userTrackingData->ip = $request->ip();
+        $userTrackingData->time_start = Carbon::now();
+        $userTrackingData->save();
+
+
+        return response()->view('welcome')->withCookie(cookie()->forever('userTrackingUuid', $uuid));
     }
 }
